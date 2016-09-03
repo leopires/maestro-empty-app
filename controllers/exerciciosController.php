23381713ec12062e3d3252fa4c97fba4e37abc4f
Leonardo Pires <lbpires@gmail.com>
2016-09-03 14:55:58 -0300

    Resposta do exercício um.

--- controllers/exerciciosController.php
+++ controllers/exerciciosController.php
@@ -3,5 +3,10 @@
 
 class ExerciciosController extends MController
 {
-
+    public function exercicioUm() {
+        $this->data->nome = "Nome: Leonardo";
+        $this->data->sobrenome = "Sobrenome: Pires";
+        $this->data->nascimento =  "Nascido em: 04/03/1985";
+        $this->render();
+    }
 }
