83f96a1aab8ea7313b4d4c9a0445752403e7ebc0
Leonardo Pires <lbpires@gmail.com>
2016-09-05 00:44:11 -0300

    Mais alguns ajustes.

--- conf/actions.php
+++ conf/actions.php
@@ -6,16 +6,15 @@ return array(
             'hello-world-html' => array('Hello World em HTML', 'hello-world/helloWorld/htmlHelloWorld', 'iconHelloWorld', '', null, array()),
             'hello-world-xml' => array('Hello World em XML', 'hello-world/helloWorld/xmlHelloWorld', 'iconHelloWorld', '', null, array()),
             'hello-world-php' => array('Hello World em PHP', 'hello-world/helloWorld/phpHelloWorld', 'iconHelloWorld', '', null, array()),
-        )
-        ),
-        'outrosHelloWorld' => array('Outros Exemplos', '', '', '', null, array(
             'hello-world-from-model' => array('Hello World de um Model', 'hello-world/helloWorld/modelHelloWorld', 'iconHelloWorld', '', null, array())
         )
         ),
         'exercicios' => array('Exercicios', '', '', '', null, array(
             'exercicio-um' => array('Exercício 01', 'hello-world/exercicios/exercicioUm', 'iconHelloWorld', '', null, array())
         )
-        )
+        ),
+        'outrosHelloWorld' => array('Outros Exemplos', '', '', '', null, array()
+        ),
     )
     )
 );
--- conf/conf.php
+++ conf/conf.php
@@ -3,7 +3,7 @@ return array(
     'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
     'name' => 'Hello World Application',
     'import' => array(),
-    'about' => array(
+    'sobre' => array(
         'autor' => 'Leonardo Pires',
         'contato' => 'leonardo.pires@ufjf.edu.br'
     )
