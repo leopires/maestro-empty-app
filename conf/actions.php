15d09154a7cbcf85b90c660f1a945c069c36fad4
Leonardo Pires <lbpires@gmail.com>
2016-09-06 02:13:27 -0300

    Exemplo de formulário.

--- conf/actions.php
+++ conf/actions.php
@@ -2,7 +2,7 @@
 
 return array(
     'hello-world-app' => array('main', '', '', '', null, array(
-        'tiposHelloWorld' => array('Tipos de Hello World', '', '', '', null, array(
+        'tiposHelloWorld' => array('Hello World', '', '', '', null, array(
             'hello-world-html' => array('Hello World em HTML', 'hello-world/helloWorld/htmlHelloWorld', 'iconHelloWorld', '', null, array()),
             'hello-world-xml' => array('Hello World em XML', 'hello-world/helloWorld/xmlHelloWorld', 'iconHelloWorld', '', null, array()),
             'hello-world-php' => array('Hello World em PHP', 'hello-world/helloWorld/phpHelloWorld', 'iconHelloWorld', '', null, array()),
@@ -10,15 +10,14 @@ return array(
         )
         ),
         'camadaApresentacao' => array('Camada de Apresentação', '', '', '', null, array(
-            'apresentacao-view-php' => array('View de um arquivo PHP', 'hello-world/views/viewFromPHPFile', 'iconHelloWorld', '', null, array()),
-            'apresentacao-view-xml' => array('View de um arquivo XML', 'hello-world/views/viewFromXMLFile', 'iconHelloWorld', '', null, array()),
+            'apresentacao-view-php' => array('View de um arquivo PHP', 'hello-world/views/viewFromPHPFile', 'iconUI', '', null, array()),
+            'apresentacao-view-xml' => array('View de um arquivo XML', 'hello-world/views/viewFromXMLFile', 'iconUI', '', null, array()),
+            'apresentacao-formulario' => array('Exemplo de Formulário', 'hello-world/views/formSample', 'iconUI', '', null, array()),
         )
         ),
         'exercicios' => array('Exercicios', '', '', '', null, array(
             'exercicio-um' => array('Exercício 01', 'hello-world/exercicios/exercicioUm', 'iconHelloWorld', '', null, array())
         )
-        ),
-        'outrosHelloWorld' => array('Outros Exemplos', '', '', '', null, array()
         ),
     )
     )
