af0a37fd33a1ab69bb6c7b92d1e145230a5d116d
Leonardo Pires <lbpires@gmail.com>
2016-09-06 16:41:05 -0300

    Ajuste para que o Manager::import funcione.

--- conf/actions.php
+++ conf/actions.php
@@ -3,20 +3,20 @@
 return array(
     'hello-world-app' => array('main', '', '', '', null, array(
         'tiposHelloWorld' => array('Hello World', '', '', '', null, array(
-            'hello-world-html' => array('Hello World em HTML', 'hello-world/helloWorld/htmlHelloWorld', 'iconHelloWorld', '', null, array()),
-            'hello-world-xml' => array('Hello World em XML', 'hello-world/helloWorld/xmlHelloWorld', 'iconHelloWorld', '', null, array()),
-            'hello-world-php' => array('Hello World em PHP', 'hello-world/helloWorld/phpHelloWorld', 'iconHelloWorld', '', null, array()),
-            'hello-world-from-model' => array('Hello World de um Model', 'hello-world/helloWorld/modelHelloWorld', 'iconHelloWorld', '', null, array())
+            'hello-world-html' => array('Hello World em HTML', 'helloWorld/htmlHelloWorld', 'iconHelloWorld', '', null, array()),
+            'hello-world-xml' => array('Hello World em XML', 'helloWorld/xmlHelloWorld', 'iconHelloWorld', '', null, array()),
+            'hello-world-php' => array('Hello World em PHP', 'helloWorld/phpHelloWorld', 'iconHelloWorld', '', null, array()),
+            'hello-world-from-model' => array('Hello World de um Model', 'helloWorld/modelHelloWorld', 'iconHelloWorld', '', null, array())
         )
         ),
         'camadaApresentacao' => array('Camada de Apresentação', '', '', '', null, array(
-            'apresentacao-view-php' => array('View de um arquivo PHP', 'hello-world/views/viewFromPHPFile', 'iconUI', '', null, array()),
-            'apresentacao-view-xml' => array('View de um arquivo XML', 'hello-world/views/viewFromXMLFile', 'iconUI', '', null, array()),
-            'apresentacao-formulario' => array('Exemplo de Formulário', 'hello-world/views/formSample', 'iconUI', '', null, array()),
+            'apresentacao-view-php' => array('View de um arquivo PHP', 'views/viewFromPHPFile', 'iconUI', '', null, array()),
+            'apresentacao-view-xml' => array('View de um arquivo XML', 'views/viewFromXMLFile', 'iconUI', '', null, array()),
+            'apresentacao-formulario' => array('Exemplo de Formulário', 'views/formSample', 'iconUI', '', null, array()),
         )
         ),
         'exercicios' => array('Exercicios', '', '', '', null, array(
-            'exercicio-um' => array('Exercício 01', 'hello-world/exercicios/exercicioUm', 'iconHelloWorld', '', null, array())
+            'exercicio-um' => array('Exercício 01', 'exercicios/exercicioUm', 'iconHelloWorld', '', null, array())
         )
         ),
     )
--- models/World.php
+++ models/World.php
@@ -1,4 +1,5 @@
 <?php
+namespace helloworld\models;
 
 class World {
 
