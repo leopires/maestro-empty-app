f8bc745fd9f45c0c59e042317762e2420cbf2f85
Leonardo Pires <lbpires@gmail.com>
2016-09-09 16:46:39 -0300

    Troca do lougar onde ficarão as configurações de conexão com o Banco de Dados.

--- conf/conf.php
+++ conf/conf.php
@@ -7,20 +7,6 @@ return array(
             'carcollection'
         )
     ),
-    'db' => array(
-        'db_car_collection' => array(
-            'driver' => 'pdo_mysql',
-            'host' => 'cursomaestro.ufjf.br',
-            'dbname' => 'db_car_collection',
-            'user' => 'root',
-            'password' => '123456',
-            'charset' => 'UTF8',
-            'formatDate' => '%d/%m/%Y %T',
-            'formatTimestamp' => '%d/%mm/%Y %T',
-            'formatDateWhere' => '%Y/%m/%d',
-            'formatTime' => '%T'
-        ),
-    ),
     'login' => array(
         'check' => false
     ),
--- modules/carcollection/conf/conf.php
+++ modules/carcollection/conf/conf.php
@@ -2,4 +2,18 @@
 
 
 return array(
+    'db' => array(
+        'db_car_collection' => array(
+            'driver' => 'pdo_mysql',
+            'host' => 'cursomaestro.ufjf.br',
+            'dbname' => 'db_car_collection',
+            'user' => 'root',
+            'password' => '123456',
+            'charset' => 'UTF8',
+            'formatDate' => '%d/%m/%Y %T',
+            'formatTimestamp' => '%d/%mm/%Y %T',
+            'formatDateWhere' => '%Y/%m/%d',
+            'formatTime' => '%T'
+        ),
+    )
 );
