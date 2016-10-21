75fab8e97effeb19522b192f3a2bd85193672f96
Leonardo Pires <lbpires@gmail.com>
2016-10-20 22:18:35 -0200

    Mais alguns ajustes.

--- conf/actions.php
+++ conf/actions.php
@@ -2,9 +2,6 @@
 
 // chave => array("Label", "action", "Imagem/Icone", "TRANSACAO", DIREITO_ACESSO , array(SUBITENS))
 
-// Manager::getAction("hello-world-app.helloWorld.hello-world-html")
-// Manager::getActions("hello-world-app.helloWorld")
-
 return array(
     'hello-world-app' => array('main', '', '', '', null, array(
         'helloWorld' => array('Hello World', '', '', '', null, array(
@@ -12,34 +9,34 @@ return array(
             'hello-world-xml' => array('Hello World em XML', 'helloWorld/xmlHelloWorld', 'iconHelloWorld', '', null, array()),
             'hello-world-php' => array('Hello World em PHP', 'helloWorld/phpHelloWorld', 'iconHelloWorld', '', null, array()),
             'hello-world-from-model' => array('Hello World de um Model', 'helloWorld/modelHelloWorld', 'iconHelloWorld', '', null, array())
-            )
+        )
         ),
         'camadaApresentacao' => array('Camada de Apresentação', '', '', '', null, array(
             'apresentacao-view-php' => array('View de um arquivo PHP', 'views/viewFromPHPFile', 'iconUI', '', null, array()),
             'apresentacao-view-xml' => array('View de um arquivo XML', 'views/viewFromXMLFile', 'iconUI', '', null, array()),
             'apresentacao-formulario' => array('Exemplo de Formulário', 'views/formSample', 'iconUI', '', null, array()),
-            )
+        )
         ),
         'camadaModel' => array('Camada Model', '', '', '', null, array(
             'carCollectionModule' => array('Car Collection', 'carcollection/main', 'iconCar', '', null, array(
                 'brand' => array('Brand', 'carcollection/brand/main', 'iconCar', '', null, array()),
                 'model' => array('Model', 'carcollection/model/main', 'iconCar', '', null, array()),
                 'car' => array('Car', 'carcollection/car/main', 'iconCar', '', null, array()),
-                'member' => array('Club Member', 'carcollection/member/main', 'iconCar', '', null, array()),
-                )
+                'member' => array('Member', 'carcollection/member/main', 'iconCar', '', null, array()),
             )
             )
+        )
         ),
         'exercicios' => array('Exercicios', '', '', '', null, array(
             'exercicio-um' => array('Exercício 01', 'exercicios/exercicioUm', 'iconSettings', '', null, array()),
             'exercicio-dois' => array('Exercício 02', 'exercicios/exercicioDois', 'iconSettings', '', null, array())
-            )
+        )
         ),
         'sobre' => array('Sobre', '', '', '', null, array(
             'sobre-hello-world-app' => array('Sobre', 'helloworld/main/sobre', '', '', null, array()),
-            )
-        ),
         )
+        ),
     )
-    );
-    ?>
+    )
+);
+?>
--- modules/carcollection/controllers/memberController.php
+++ modules/carcollection/controllers/memberController.php
@@ -18,7 +18,7 @@ class MemberController extends \MController {
     }
 
     /**
-     *
+     * Action que exibe o formulário de listagem e pesquisa de Membros.
      */
     public function formFind() {
 
@@ -28,7 +28,7 @@ class MemberController extends \MController {
             $this->data->members = Member::create()->listByFirstNameOrEMailOrderByFirstNameAsc($firstName, $email)->asQuery();
             $this->render();
         }catch (Exception $ex) {
-
+            $this->renderPrompt(MPrompt::MSG_TYPE_ERROR, "An error occurs while trying to list the Members.", self::MODULE_MAIN);
         }
     }
 
@@ -59,12 +59,13 @@ class MemberController extends \MController {
             // 3 - Ação a ser executada ao clicar no botão. Esse ação é o caminho para uma action.
             $this->renderPrompt(MPrompt::MSG_TYPE_INFORMATION, "Member '{$member->getDescription()}' saved with success!", self::ACTION_FORM_FIND);
         } catch (EDataValidationException $ex) {
+            // Quando alguma das validações do model falha. Ver: Member.php, método public static function config().
             $this->renderPrompt(MPrompt::MSG_TYPE_ERROR, $ex->getMessage(), self::ACTION_FORM_FIND);
         } catch (Exception $ex) {
             // Informa ao usuário que ocorreu um erro ao salvar os dados.
             // Obs. Não é uma boa prática exibir mensagens de erro com conteúdo técnico para o usuário.
-            // -- O usuário não vai entender o que está acontecendo.
-            // -- Mensagens técnicas podem expor falhas no sistema que a serem exploradas por um eventual atacante.
+            // - O usuário não vai entender o que está acontecendo.
+            // - Mensagens técnicas podem expor falhas no sistema a serem exploradas por um eventual atacante.
             $this->renderPrompt(MPrompt::MSG_TYPE_ERROR, "An error occurs while trying to save the Member data.", self::ACTION_FORM_FIND);
         }
     }
